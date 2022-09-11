31bdfc1ee8219dc56246ea7d01134391ca7c1d80
Grohot9 <igor.samarsky8@gmail.com>
2022-09-11 08:03:14 +0300

    Publication of the homework based on the fifth lesson

---
+++ homework_lesson_5.py
@@ -0,0 +1,7 @@
+# 1) У вас есть строка my_string = '0123456789'.
+# Сгенерировать целые числа (тип int) от 0 до 99 и распечатать их.
+
+my_string = "0123456789"
+for num_1 in my_string:
+    for num_2 in my_string:
+        print(int(num_2) if int(num_1 + num_2) < 10 else int(num_1 + num_2))
