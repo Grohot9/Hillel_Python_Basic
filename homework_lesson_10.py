70c30b3b6ad673871f2ba06dacb16007851a6519
Grohot9 <igor.samarsky8@gmail.com>
2022-10-02 09:44:44 +0300

    Returned the code to its original form and added the use of the previous function to the 4th task.

--- homework_lesson_10.py
+++ homework_lesson_10.py
@@ -12,8 +12,6 @@ get_domains("domains.txt")
 # и возвращает список всех фамилий из него.
 # Каждая строка файла содержит номер, фамилию, страну, некоторое число (таблица взята с википедии).
 # Разделитель - символ табуляции "\t"
-
-
 def get_surnames(file_name: str) -> list:
     with open(file_name, "r") as names:
         surnames = [line_info.split()[1] for line_info in names.readlines()]
@@ -26,8 +24,6 @@ get_surnames("names.txt")
 # словарей вида {"date": date}
 # в которых date - это дата из строки (если есть),
 # Например [{"date": "1st January 1919"}, {"date": "8th February 1828"},  ...]
-
-
 def get_dates(file_name: str) -> list:
     dates = []
     with open(file_name, "r") as file:
@@ -37,7 +33,7 @@ def get_dates(file_name: str) -> list:
     return dates
 
 
-print(get_dates("authors.txt"))
+get_dates("authors.txt")
 
 # По просьбам некоторых студентов начну включать дополнительные задания.
 # 4* (*сдавать не обязательно, но если будете сдавать, то ошибки будут учитываться тоже).
@@ -46,43 +42,37 @@ print(get_dates("authors.txt"))
 # в которых date_original - это дата из строки (если есть),
 # а date_modified - это та же дата, представленная в формате "dd/mm/yyyy" (d-день, m-месяц, y-год)
 # Например [{"date_original": "8th February 1828", "date_modified": 08/02/1828},  ...]
-
-
-def get_original_and_modified_dates_decorator(func):
-    def inner(*args, **kwargs):
-        months = {"January": "01",
-                  "February": "02",
-                  "March": "03",
-                  "April": "04",
-                  "May": "05",
-                  "June": "06",
-                  "July": "07",
-                  "August": "08",
-                  "September": "09",
-                  "October": "10",
-                  "November": "11",
-                  "December": "12"
-                  }
-        dates = [{"date_original": date["date"]} for date in func(*args, **kwargs)]
-        for date in dates:
-            date_modified_year = date["date_original"].split()[-1]
-            date_modified_month = months[date["date_original"].split()[-2]]
-            if len(date["date_original"].split()) > 2:
-                if len(date["date_original"].split()[-3]) == 3:
-                    date_modified_day = "0" + date["date_original"].split()[-3][0]
-                else:
-                    date_modified_day = date["date_original"].split()[-3][0:2]
+def get_original_and_modified_dates(file_name: str) -> list:
+    months = {"January": "01",
+              "February": "02",
+              "March": "03",
+              "April": "04",
+              "May": "05",
+              "June": "06",
+              "July": "07",
+              "August": "08",
+              "September": "09",
+              "October": "10",
+              "November": "11",
+              "December": "12"
+              }
+    dates = [{"date_original": date["date"]} for date in get_dates(file_name)]
+    for date in dates:
+        date_modified_year = date["date_original"].split()[-1]
+        date_modified_month = months[date["date_original"].split()[-2]]
+        if len(date["date_original"].split()) > 2:
+            if len(date["date_original"].split()[-3]) == 3:
+                date_modified_day = "0" + date["date_original"].split()[-3][0]
             else:
-                date_modified_day = "__"
-            date_modified = {"date_modified": f"{date_modified_day}/{date_modified_month}/{date_modified_year}"}
-            date.update(date_modified)
-        return dates
-    return inner
-
-
+                date_modified_day = date["date_original"].split()[-3][0:2]
+        else:
+            date_modified_day = "__"
+        date_modified = {"date_modified": f"{date_modified_day}/{date_modified_month}/{date_modified_year}"}
+        date.update(date_modified)
+    return dates
 # Пришлось продумывать такую логику так как в одной из строк в файле authors.txt есть случай где указывается только
 # месяц и год и я не знаю было ли так задумано или просто пропустили день в дате, так что сделал для этого случая
 # вот такой вывод: {'date_original': 'December 1817', 'date_modified': '__/12/1817'}
 
-modified_dates = get_original_and_modified_dates_decorator(get_dates)
-print(modified_dates("authors.txt"))
+
+get_original_and_modified_dates("authors.txt")
