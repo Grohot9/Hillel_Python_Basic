2420cfd23f75c4434146a57e23f3371524a5be26
Grohot9 <igor.samarsky8@gmail.com>
2022-10-23 23:04:34 +0200

    Bug fix

--- homework_10/homework_lesson_15.py
+++ homework_10/homework_lesson_15.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import random
 
 
 def sort_by_surname(item: dict) -> str:
@@ -15,25 +16,33 @@ class Quotes:
         self._url = "http://api.forismatic.com/api/1.0/"
         self._quotes = []
 
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def quotes(self):
+        return self._quotes
+
     def get_quotes(self):
         for quote_num in range(self.quotes_count):
-            self._quotes += [self.get_quote(key=quote_num)]
+            self._quotes += [self.get_quote()]
 
-    def get_quote(self, key: int):
+    def get_quote(self):
         params = {
             "method": "getQuote",
             "format": self.format,
-            "key": key,
+            "key": random.randint(0, 999999),
             "lang": "en"
         }
         response = requests.get(self._url, params=params)
         try:
-            if response.json() is None:
-                return self.get_quote(key)
+            if response.json()["quoteAuthor"].strip() == "":
+                return self.get_quote()
             else:
                 return response.json()
         except json.decoder.JSONDecodeError:
-            return self.get_quote(key)
+            return self.get_quote()
 
     def print_quotes(self):
         for quote in self._quotes:
@@ -48,11 +57,11 @@ class Quotes:
 
 # Usage:
 # if __name__ == "__main__":
-#     quotes_count = 5
-#     filename = "test"
-#     file_format = "json"
-#
-#     test = Quotes(quotes_count=quotes_count, filename=filename, file_format=file_format)
-#     test.get_quotes()
-#     test.print_quotes()
-#     test.save_quotes()
+    # quotes_count = 100
+    # filename = "test"
+    # file_format = "json"
+    #
+    # test = Quotes(quotes_count=quotes_count, filename=filename, file_format=file_format)
+    # test.get_quotes()
+    # test.print_quotes()
+    # test.save_quotes()
