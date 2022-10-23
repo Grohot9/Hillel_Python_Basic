f332e20874a6d69d183bec4b6d679767907ecc49
Grohot9 <igor.samarsky8@gmail.com>
2022-10-23 15:45:04 +0200

    Add class realisation

--- homework_07/extra_homework/lesson_10_thanos.py
+++ homework_07/extra_homework/lesson_10_thanos.py
@@ -1,41 +1,44 @@
+# 1. Создать папку ./alphabet/ Если папка существует, то ОК.
+# 2. В папке ./alphabet/ создать файлы вида a.txt, b.txt, ..., z.txt
+# в которых будет записана строка алфавита, но с заменой буквы из названия файла на прописную.
+# Пример: для b.txt строка будет aBcde...
+# 3. Сделать щелчок Таноса - удалить случайным образом половину всех файлов в этой папке.
 import os
 import string
 import random
 
 
-# 1. Создать папку ./alphabet/ Если папка существует, то ОК.
-location = "./"
-file = "alphabet"
-cwd = location + file
+class ThanosSnap:  # with encapsulation examples
+    def __init__(self, dirname: str):
+        self._dirname = dirname
+        self._create_directory()
+
+    @property
+    def dirname(self):
+        return self._dirname
+
+    def _create_directory(self):
+        os.makedirs(self._dirname, exist_ok=True)
+
+    def crate_files(self):
+        for symbol in string.ascii_lowercase:
+            filename = os.path.join(self._dirname, f"{symbol}.txt")
+            self._create_data_in_file(filename, symbol)
+
+    @staticmethod
+    def _create_data_in_file(filename: str, symbol: str):
+        data = string.ascii_lowercase.replace(symbol, symbol.upper())
+        with open(filename, "w") as file:
+            file.write(data)
+
+    def do_thanos_snap(self):
+        listdir = os.listdir(self._dirname)
+        random.shuffle(listdir)
+        for filename in listdir[:len(listdir) // 2]:
+            os.remove(os.path.join(self._dirname, filename))
 
 
-def create_file(path: str = cwd):
-    try:
-        os.mkdir(path)
-    except FileExistsError:
-        pass
-
-
-create_file()
-# 2. В папке ./alphabet/ создать файлы вида a.txt, b.txt, ..., z.txt
-# в которых будет записана строка алфавита, но с заменой буквы из названия файла на прописную.
-# Пример: для b.txt строка будет aBcde...
-os.chdir(cwd)
-
-for character in string.ascii_lowercase:
-    with open(f"{character}.txt", "w") as file:
-        file.write(string.ascii_lowercase.replace(character, character.upper()))
-# 3. Сделать щелчек Таноса - удалить случайным образом половину всех файлов в этой папке.
-# files_for_delete = random.choices(string.ascii_lowercase.split(), k=len(string.ascii_lowercase) // 2)
-# for del_file in files_for_delete:
-#     os.remove(f"{location}{file}/{del_file}.txt")
-
-
-def thanos_snap():
-    for root, dirs, files in os.walk("../.."):
-        files_for_delete = random.sample(files, k=len(files) // 2)
-        for file_for_delete in files_for_delete:
-            os.remove(file_for_delete)
-
-
-thanos_snap()
+directory_name = "alphabet"
+example = ThanosSnap(directory_name)
+example.crate_files()
+example.do_thanos_snap()
